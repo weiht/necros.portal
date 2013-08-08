b4afffc7f0e12bbe6518b3f59bbfeeb6d58d200c
weiht <weihthchk@hotmail.com>
2013-08-08 14:13:46 +0800

    Beans from the core package defined.

--- necros-portal-core/src/main/java/org/necros/portal/org/CharRangePasswordGenerator.java
+++ necros-portal-core/src/main/java/org/necros/portal/org/CharRangePasswordGenerator.java
@@ -4,6 +4,7 @@
 package org.necros.portal.org;
 
 import java.util.List;
+import java.util.Random;
 
 import org.apache.commons.lang.CharRange;
 
@@ -16,10 +17,21 @@ public class CharRangePasswordGenerator implements PasswordGenerator {
 	
 	private List<CharRange> ranges;
 	private char[] chars = null;
+	private int passwordLength = 8;
 	
 	public String generate() {
 		ensureChars();
-		return null;
+		return doGenerate();
+	}
+
+	private String doGenerate() {
+		int len = passwordLength;
+		Random rnd = new Random();
+		StringBuilder buff = new StringBuilder(len);
+		for (int i = 0; i < len; i ++) {
+			buff.append(chars[rnd.nextInt(chars.length)]);
+		}
+		return buff.toString();
 	}
 
 	private synchronized void ensureChars() {
@@ -43,5 +55,13 @@ public class CharRangePasswordGenerator implements PasswordGenerator {
 
 	public void setRanges(List<CharRange> ranges) {
 		this.ranges = ranges;
+	}
+
+	public int getPasswordLength() {
+		return passwordLength;
+	}
+
+	public void setPasswordLength(int passwordLength) {
+		this.passwordLength = passwordLength;
 	}
 }
