6bf9a73dcbaa9d89bfe779a7bcd5479b6104ac75
weiht <weihthchk@gmail.com>
2013-09-04 21:17:26 +0800

    实现了单位和人员的控制器。

--- necros-portal-core/src/main/java/org/necros/portal/org/web/OrgController.java
+++ necros-portal-core/src/main/java/org/necros/portal/org/web/OrgController.java
@@ -1,11 +1,14 @@
 package org.necros.portal.org.web;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.necros.portal.org.Organization;
 import org.necros.portal.org.OrganizationService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,18 +21,28 @@ public class OrgController {
 	@Resource(name="c.menuService")
 	private OrganizationService orgService;
 
+	@RequestMapping(value=BASE_CTX_URL + "/children/:id", method=RequestMethod.GET)
+	public @ResponseBody List<Organization> listChildren(@PathVariable String id, HttpServletRequest req) {
+		return orgService.children(id);
+	}
+
+	@RequestMapping(value=BASE_CTX_URL + "/get/:id", method=RequestMethod.GET)
+	public @ResponseBody Organization getOrg(@PathVariable String id, HttpServletRequest req) {
+		return orgService.get(id);
+	}
+
 	@RequestMapping(value=BASE_CTX_URL + "/add", method=RequestMethod.POST)
-	public @ResponseBody Organization addOrg(@RequestBody Organization itm, HttpServletRequest req) {
-		return orgService.create(itm, SessionContext.getCurrentContext(req).getCurrentUser());
+	public @ResponseBody Organization addOrg(@RequestBody Organization org, HttpServletRequest req) {
+		return orgService.create(org, SessionContext.getCurrentContext(req).getCurrentUser());
 	}
 
 	@RequestMapping(value=BASE_CTX_URL + "/update", method=RequestMethod.POST)
-	public @ResponseBody Organization updateOrg(@RequestBody Organization itm, HttpServletRequest req) {
-		return orgService.update(itm, SessionContext.getCurrentContext(req).getCurrentUser());
+	public @ResponseBody Organization updateOrg(@RequestBody Organization org, HttpServletRequest req) {
+		return orgService.update(org, SessionContext.getCurrentContext(req).getCurrentUser());
 	}
 
 	@RequestMapping(value=BASE_CTX_URL + "/remove", method=RequestMethod.POST)
-	public @ResponseBody Organization removeOrg(@RequestBody Organization itm, HttpServletRequest req) {
-		return orgService.remove(itm.getId(), SessionContext.getCurrentContext(req).getCurrentUser());
+	public @ResponseBody Organization removeOrg(@RequestBody Organization org, HttpServletRequest req) {
+		return orgService.remove(org.getId(), SessionContext.getCurrentContext(req).getCurrentUser());
 	}
 }
--- necros-portal-core/src/main/java/org/necros/portal/org/web/PersonController.java
+++ necros-portal-core/src/main/java/org/necros/portal/org/web/PersonController.java
@@ -1,9 +1,18 @@
 package org.necros.portal.org.web;
 
-import javax.annotation.Resource;
+import java.util.List;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.necros.portal.org.Person;
 import org.necros.portal.org.PersonService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PersonController {
@@ -11,4 +20,29 @@ public class PersonController {
 	
 	@Resource(name="c.menuService")
 	private PersonService personService;
+
+	@RequestMapping(value=BASE_CTX_URL + "/all", method=RequestMethod.GET)
+	public @ResponseBody List<Person> allPersons(@PathVariable String id, HttpServletRequest req) {
+		return personService.all();
+	}
+
+	@RequestMapping(value=BASE_CTX_URL + "/get/:id", method=RequestMethod.GET)
+	public @ResponseBody Person getPerson(@PathVariable String id, HttpServletRequest req) {
+		return personService.get(id);
+	}
+
+	@RequestMapping(value=BASE_CTX_URL + "/add", method=RequestMethod.POST)
+	public @ResponseBody Person addPerson(@RequestBody Person p, HttpServletRequest req) {
+		return personService.create(p, SessionContext.getCurrentContext(req).getCurrentUser());
+	}
+
+	@RequestMapping(value=BASE_CTX_URL + "/update", method=RequestMethod.POST)
+	public @ResponseBody Person updatePerson(@RequestBody Person p, HttpServletRequest req) {
+		return personService.update(p, SessionContext.getCurrentContext(req).getCurrentUser());
+	}
+
+	@RequestMapping(value=BASE_CTX_URL + "/remove", method=RequestMethod.POST)
+	public @ResponseBody Person removePerson(@RequestBody Person p, HttpServletRequest req) {
+		return personService.remove(p.getId(), SessionContext.getCurrentContext(req).getCurrentUser());
+	}
 }
